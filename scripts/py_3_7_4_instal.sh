8fbe33d738f93b6f2f25aa9bb438c62ce5384023
John Murray <jmurrayufo@gmail.com>
2019-10-28 16:58:34 -0600

    Add sqlite extenstions support

--- scripts/py_3_7_4_instal.sh
+++ scripts/py_3_7_4_instal.sh
@@ -9,7 +9,7 @@ rm -rf Python-3.7.4*
 wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
 tar xzvf Python-3.7.4.tgz
 cd Python-3.7.4
-./configure --enable-optimizations
+./configure --enable-optimizations --enable-loadable-sqlite-extensions
 make
 make altinstall
 /usr/local/bin/pip3.7 install --upgrade pip
